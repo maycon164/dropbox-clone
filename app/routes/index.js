3f8aa14e11e749aad14ce92fbad873b0a5613972
maycon164 <mayconfelipe164@gmail.com>
2021-09-08 14:19:14 -0300

    criando rota /upload, recebendo e salvando arquivos com formidable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload', (req, res) => {
+  
+  let form = formidable({
+    uploadDir: './uploadFiles',
+    keepExtensions: true,
+    multiples: true
+  });
+
+  form.parse(req, (error, fields,  files) => {
+    console.log(error);
+    console.log(fields);
+    console.log(files);
+  });
+
+});
+
 
 module.exports = router;

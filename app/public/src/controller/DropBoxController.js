009d3b58e004d9a7fe5eb279d69c371ee445741c
maycon164 <mayconfelipe164@gmail.com>
2021-09-08 21:00:40 -0300

    progress bar

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -4,7 +4,14 @@ class DropBoxController{
         
         this.btnSendFilesEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
+
+        //BARRA DE PROGRESSO
         this.snackModalBar = document.querySelector('#react-snackbar-root');
+
+        this.fileName = this.snackModalBar.querySelector('.filename');
+        this.timeLeft = this.snackModalBar.querySelector('.timeleft');
+        this.barProgress = this.snackModalBar.querySelector('.mc-progress-bar-fg');
+
 
         this.initEvents();
 
@@ -20,9 +27,14 @@ class DropBoxController{
         this.inputFilesEl.addEventListener('change', event => {
             
             this.uploadFilesTask(event.target.files).then(resp => console.log(resp));
-
-            this.snackModalBar.style.display = 'block';
+            this.modalShow();
+            this.inputFilesEl.value = '';
+            
         });
+    }
+
+    modalShow(show = true){
+        this.snackModalBar.style.display = (show) ? "block" : "none";
     }
 
     uploadFilesTask(files){
@@ -38,16 +50,27 @@ class DropBoxController{
                     ajax.open('POST', '/upload');
     
                     ajax.onload = event => {
-                        
+                        this.modalShow(false);
                         try{
                             resolve(JSON.parse(ajax.responseText));
                         }catch(error){
+                            
                             reject(error);
                         }
     
                     }
+
+                    ajax.upload.onprogress = event => {
+                        
+                        this.uploadProgress(event, file);
+                        this.startTimeUpload = Date.now();
+
+                    }
     
-                    ajax.onerror = error => reject(error);
+                    ajax.onerror = error => {
+                        this.modalShow();  
+                        reject(error);
+                    }
     
                     let formData = new FormData();
                     formData.append('input-file', file);
@@ -59,4 +82,40 @@ class DropBoxController{
         return Promise.all(filesPromises);
     }
 
+    uploadProgress(event, file){
+
+        let timeSpent = Date.now() - this.startTimeUpload;
+
+        let loaded = event.loaded;
+        let total = event.total;
+    
+        let percent = parseInt((loaded / total) * 100);
+    
+        let timeLeft = ((100 - percent) * timeSpent) / percent;
+
+        console.log(timeSpent, timeLeft, percent);
+
+        this.barProgress.style.width = `${percent}%`;
+        this.fileName.innerHTML = file.name;
+        this.timeLeft.innerHTML = this.timeToHuman(timeLeft);
+
+    }
+
+    timeToHuman(duration){
+        
+        let seg = parseInt((duration / 1000) % 60);
+        let min = parseInt( (duration / (1000 * 60)) % 60);
+        let hour = parseInt( (duration / (1000 * 60 * 60) ) % 24);
+        
+        if(hour > 0){ 
+            return `${hour} horas ${min} minutos e ${seg} segundos`;
+        }
+        if(min > 0) {
+            return `${min} minutos e ${seg} segundos`;
+        }
+        if(seg > 0){ 
+            return `${seg} segundos`;
+        }
+        return `${hour} horas ${min} minutos e ${seg} segundos`;
+    }
 }
